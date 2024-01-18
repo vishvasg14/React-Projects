331898fd987e3351df1605b8db2f0284b6290806
Vishvas Gohil <vishvasg14@gmail.com>
2024-01-18 14:50:41 +0530

    navbar changes

--- top-courses/src/components/Navbar.js
+++ top-courses/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="bg-gray-900 py-4">
-        <h1 className="texr-3xl font-bold text-center text-white">Top Movies</h1>
+        <h1 className="texr-3xl font-bold text-center text-white">TOP COURSES</h1>
       </nav>
     </div>
   );
